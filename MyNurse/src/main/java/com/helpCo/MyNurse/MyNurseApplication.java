6a6af799e4356cafd5f742750eafea46b765ae08
Jeff Mitsou <shinetachi88@gmail.com>
2022-01-03 17:18:23 -0500

    Initial Commit

---
+++ MyNurse/src/main/java/com/helpCo/MyNurse/MyNurseApplication.java
@@ -0,0 +1,13 @@
+package com.helpCo.MyNurse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNurseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNurseApplication.class, args);
+	}
+
+}
